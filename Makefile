b7ea104b4b32952c4bfd7eaff6ea8f7772f0d2b7
Michael Jansen <kde@michael-jansen.biz>
2014-08-15 20:49:28 +0200

    Initial project setup.

---
+++ Makefile
@@ -0,0 +1,15 @@
+NAME=env
+
+setup: ez_setup.py $(NAME)
+	@echo "  - Installing setuptools"
+	wget https://bootstrap.pypa.io/ez_setup.py -O - | $(NAME)/bin/python
+	@echo "  - Installing pip"
+	$(NAME)/bin/easy_install pip
+
+$(NAME):
+	@echo "* Creating virtual environment "
+	pyvenv $(NAME)
+
+ez_setup.py:
+	wget https://bootstrap.pypa.io/ez_setup.py
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+	name = "HelloWorld",
+	version = "0.1",
+	packages = find_packages(),
+
+	# metadata for upload to PyPI
+	author = "Michael Jansen",
+	author_email = "info@michael-jansen.biz",
+	description = "TODO",
+	license = "PSF",
+	keywords = "hello world example examples",
+	url = "http://example.com/HelloWorld/",   # project home page, if any
+)
